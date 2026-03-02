b94d775b3a3e5214f909384b0a23f7c5efcd63cc
Vamshi1564 <krishnavamshi927917@gmail.com>
2026-03-02 16:19:20 +0530

    updated

--- Routes/PropertyRoutes.js
+++ Routes/PropertyRoutes.js
@@ -91,8 +91,8 @@ router.post("/add-property", uploadFields, async (req, res) => {
             `INSERT INTO properties
       (name, category, city, area, pincode, address, landmark,
        contact, email, supplier_id,
-       hotel_remarks, registration_certificate)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+        registration_certificate)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
             [
                 name,
                 category,
@@ -104,7 +104,6 @@ router.post("/add-property", uploadFields, async (req, res) => {
                 contact || "",
                 email || "",
                 supplier_id,
-                parsedPolicies.hotel_remarks || "",
                 req.files?.certificate?.[0]?.filename || ""
             ]
         );
@@ -117,7 +116,7 @@ router.post("/add-property", uploadFields, async (req, res) => {
                 propertyId,
                 file.filename,
                 index == Number(coverIndex) ? 1 : 0
-            ]);
+            ]); rackRate
 
             await connection.query(
                 `INSERT INTO property_images
